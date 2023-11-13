31c00259e83b52153e7f4fcc214aa577845bcf3a
honeydanji <tjdwls3680@naver.com>
2023-11-13 23:25:46 +0900

    FEAT : 에러 메세지 상수 선언
    // 완료
    - 상수는 static 으로 선언
    - 생성자 캡슐화로 private 선언

---
+++ src/main/java/christmas/utils/ExceptionMessage.java
@@ -0,0 +1,13 @@
+package christmas.utils;
+
+public class ExceptionMessage {
+
+    private ExceptionMessage() {}
+
+    static final String NOT_NUMERIC = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    static final String OUT_OF_RANGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    static final String MISMATCHED_MENU = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    static final String REPEATED_MENU = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    static final String NON_EXISTENT_MENU = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    static final String INVALID_MENU_COUNT = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+}
