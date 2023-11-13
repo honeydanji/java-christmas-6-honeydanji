1e9e9bdbc4974ce0955d5e2b7953ea1600cb2e42
honeydanji <tjdwls3680@naver.com>
2023-11-14 00:17:04 +0900

    FEAT : 메뉴 입력에 대한 예외 코드 작성 및 테스트 진행
    // 완료
    - 형식에 맞지 않게 입력한 경우 - 테스트 완료
    - 반복된 메뉴를 입력한 경우 - 테스트 완료
    - 존재하지 않는 메뉴를 입력한 경우 - 테스트 완료
    - 메뉴를 1개 미만으로 입력한 경우 - 테스트 완료
    - 메뉴를 20개 초과 입력한 경우 - 테스트 완료
    - 형식에 맞게 메뉴 여러개 입력한 경우 - 테스트 완료
    - 형식에 맞게 메뉴 1개만 입력한 경우 - 테스트 완료
    
    // TIP
    - 정규표현식을 활용해서 입력 형식에 대한 예외를 처리
    - 정해진 메뉴를 foodDto 메서드로 표현으로 코드 라인수 감소
    - HashSet 이용으로 인한 중복 체크 및 가독성 증가
    
    >> 일급컬렉션을 최대한 활용해서 코드를 효율적으로 작성하려고 노력

---
+++ src/main/java/christmas/utils/InputMenuException.java
@@ -0,0 +1,69 @@
+package christmas.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static christmas.utils.ExceptionMessage.*;
+
+public class InputMenuException {
+
+    public void preConversionException(String foodAndAmount) {
+        mismatchedMenu(foodAndAmount);
+        repeatedMenu(foodAndAmount);
+    }
+
+    public void postConversionException(HashMap<String, Integer> foodAndAmount) {
+        nonExistentMenu(foodAndAmount);
+        invalidMenuCount(foodAndAmount);
+    }
+
+    private void mismatchedMenu(String foodAndAmount) {
+        // 정규 표현식 패턴
+        String regex = "^(\\p{L}+-\\d+)(,\\s*\\p{L}+-\\d+)*$";
+
+        if (!Pattern.matches(regex, foodAndAmount)) {
+            throw new IllegalArgumentException(MISMATCHED_MENU);
+        }
+    }
+
+    private void repeatedMenu(String foodAndAmount) {
+        String[] splitText = foodAndAmount.split("[\\s,\\d-]+");
+        HashSet<String> duplicatedCheck = new HashSet<>();
+        for(String s : splitText) {
+            duplicatedCheck.add(s);
+        }
+        if(splitText.length != duplicatedCheck.size()) {
+            throw new IllegalArgumentException(REPEATED_MENU);
+        }
+    }
+
+    private void nonExistentMenu(HashMap<String, Integer> foodAndAmount) {
+        List<String> foodCheck = new ArrayList<>(foodDto());
+
+        for(String s : foodAndAmount.keySet()) {
+            if (!foodCheck.contains(s)) {
+                throw new IllegalArgumentException(NON_EXISTENT_MENU);
+            }
+        }
+    }
+
+    private void invalidMenuCount(HashMap<String, Integer> foodAndAmount) {
+        int total = 0;
+        for(String food : foodAndAmount.keySet()) {
+            total += foodAndAmount.get(food);
+        }
+        if(total > 20 || total < 1) {
+            throw new IllegalArgumentException(INVALID_MENU_COUNT);
+        }
+    }
+
+    private List<String> foodDto() {
+        return List.of("양송이스프","타파스","시저샐러드",
+                        "티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타",
+                        "초코케이크","아이스크림",
+                        "제로콜라","레드와인","샴페인");
+    }
+}
