5b1ac7f99f9d45957ad6398f2bc6e5eb56a405a3
honeydanji <tjdwls3680@naver.com>
2023-11-13 14:06:35 +0900

    FEAT : 날짜 입력에 대한 예외 코드 구현
    // 완료
    - 숫자가 아닌 경우
    - 지정한 범위를 넘어간 경우
    >> 캡슐화를 위해서 totalException() 을 제외한 모든 메서드는 private 로 선언
    
    // 추가 사항
    - 입력 클래스 생성 후 제공된 테스트 진행

---
+++ src/main/java/christmas/utils/InputDateException.java
@@ -0,0 +1,26 @@
+package christmas.utils;
+
+public class InputDateException {
+
+    public void totalException(String date) {
+        notNumeric(date);
+        OutOfRange(date);
+    }
+
+    // 숫자 문자열 변환
+    private void notNumeric(String date) {
+        try {
+            Integer.parseInt(date);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    // 숫자범위
+    private void OutOfRange(String date) {
+        int dateChange = Integer.parseInt(date);
+        if(dateChange <= 0 && dateChange > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+}
