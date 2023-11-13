4db610c4748b3f0ee0b8b6faa26a60f8593122b0
honeydanji <tjdwls3680@naver.com>
2023-11-13 15:38:16 +0900

    FEAT : 메뉴 입력에 대한 함수 작성
    // 완료
    - 예외 처리 함수 호출 및 재입력에 관한 코드 작성
    - 메뉴 입력 타입변환 함수 추가(String > HashMap)
    >> HashMap 은 key : value 로 표현이 가능.
    >> 빠른 데이터 검색이 가능.
    - 예외 처리에 대해서 타입변환 전과 후로 나뉨
    >> 기본적으로 제공된 4가지 테스트를 통과하기 위함
    
    // 추가 사항
    - 메뉴 입력에 대한 예외 처리 코드 초가
    - 테스트 진행 및 테스트 코드 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,11 +2,15 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.utils.InputDateException;
+import christmas.utils.InputMenuException;
+
+import java.util.HashMap;
 
 import static christmas.utils.InputMessage.*;
 
 public class InputView {
     private InputDateException inputDateException = new InputDateException();
+    private InputMenuException inputMenuException = new InputMenuException();
 
     // 날짜 입력 받기
     public int inputDate() {
@@ -23,5 +27,31 @@ public class InputView {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public HashMap<String, Integer> inputFoodAndAmount() {
+        System.out.println(INPUT_MENU);
+        while (true) {
+            try {
+                String input = Console.readLine();
+                inputMenuException.preConversionException(input); // 타입변환 전 예외 검사 2개
+                HashMap<String, Integer> foodAndAmount = typeChange(input);
+                inputMenuException.postConversionException(foodAndAmount); // 타입변환 후 예외 검사 2개
+                return foodAndAmount;
+            }catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private HashMap<String, Integer> typeChange(String input) {
+        HashMap<String, Integer> foodAndAmount = new HashMap<>();
+
+        String[] oneProcess = input.split(",");
+        for(String a : oneProcess) {
+            String[] twoProcess = a.split("-");
+            foodAndAmount.put(twoProcess[0],Integer.parseInt(twoProcess[1]));
+        }
+        return foodAndAmount;
     }
 }
