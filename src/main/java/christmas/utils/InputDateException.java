fb1d8f66d4e963d32a3417efa1e67a18074b2a10
honeydanji <tjdwls3680@naver.com>
2023-11-13 14:22:10 +0900

    REFACTOR : 날짜 입력 범위 벗어난 경우 조건문 수정
    // 수정
    - if(dateChange <= 0 && dateChange > 31) > if(dateChange <= 0 || dateChange > 31)

--- src/main/java/christmas/utils/InputDateException.java
+++ src/main/java/christmas/utils/InputDateException.java
@@ -19,7 +19,7 @@ public class InputDateException {
     // 숫자범위
     private void OutOfRange(String date) {
         int dateChange = Integer.parseInt(date);
-        if(dateChange <= 0 && dateChange > 31) {
+        if(dateChange <= 0 || dateChange > 31) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
