36c9452ca1115ef882a0c213b47e1f2030e2829c
honeydanji <tjdwls3680@naver.com>
2023-11-14 10:56:32 +0900

    REFACTOR : NOT_DISCOUNT_PAYMENT 상수 추가

--- src/main/java/christmas/utils/OutputMessage.java
+++ src/main/java/christmas/utils/OutputMessage.java
@@ -5,6 +5,7 @@ public enum OutputMessage {
     PREVIEW("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n"),
     ORDER_MENU("<주문 메뉴>\n"),
     MENU("%s %d개"),
+    NOT_DISCOUNT_PAYMENT("할인 전 총주문 금액\n" + "%d원"),
     GIFT_MENU("<증정 메뉴>\n" + "샴페인 %d"),
     BENEFIT_DETAIL("<혜택 내역>\n"
                     + "크리스마스 디데이 할인: -%d원\n"
