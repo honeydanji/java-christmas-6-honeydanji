1a74b359065047d19c85fa8c9a6630aa3a3118ba
honeydanji <tjdwls3680@naver.com>
2023-11-14 15:51:04 +0900

    FEAT : 예약 날짜에 대한 D-DAY 할인 적용 클래스 추가
    // 완료
    - 디데이 아닌 날 테스트 코드 작성 및 통과
    - 디데이 당일 날 테스트 코드 작성 및 통과
    
    // 추가 사항
    - 평일 할인 클래스 및 테스트 코드 작성

--- src/main/java/christmas/model/EventBenefit.java
+++ src/main/java/christmas/model/EventBenefit.java
@@ -1,6 +1,7 @@
 package christmas.model;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class EventBenefit {
     private int reservationDate;
@@ -19,8 +20,19 @@ public class EventBenefit {
         }
         return false;
     }
-    // 할인 금액
-    // 혜택 금액
-    // 뱃지
-    // 최종 결제 금액
+
+    public int christmasDiscount() {
+        int discount = 0;
+        if (christmasPeriod().contains(reservationDate)) {
+            discount = 1000 + 100 * (reservationDate - 1);
+            return discount;
+        }
+        return discount;
+    }
+
+    private List<Integer> christmasPeriod() {
+        List<Integer> christmasPeriod = List.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25);
+        return christmasPeriod;
+    }
+
 }
--- src/test/java/christmas/model/EventBenefitTest.java
+++ src/test/java/christmas/model/EventBenefitTest.java
@@ -32,4 +32,24 @@ public class EventBenefitTest {
         assertEquals(false,eventBenefit.gift());
     }
 
+    @Test
+    void 디데이_할인_테스트() {
+        int reservationDateTest = 1;
+        HashMap<String, Integer> foodAndAmountTest = new HashMap<>();
+        int totalPaymentTest = 110000;
+        eventBenefit = new EventBenefit(reservationDateTest, foodAndAmountTest, totalPaymentTest);
+
+        assertEquals(1000, eventBenefit.christmasDiscount());
+    }
+
+    @Test
+    void 디데이_아닌_날_할인_테스트() {
+        int reservationDateTest = 27;
+        HashMap<String, Integer> foodAndAmountTest = new HashMap<>();
+        int totalPaymentTest = 110000;
+        eventBenefit = new EventBenefit(reservationDateTest, foodAndAmountTest, totalPaymentTest);
+
+        assertEquals(0,eventBenefit.christmasDiscount());
+    }
+
 }
